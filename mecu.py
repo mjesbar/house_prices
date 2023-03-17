0ef43c818887c04e454d4cea21be626b22bfe1d2
Debian <debian@debian11.linuxvmimages.local>
2023-03-17 18:10:10 -0500

    ~

--- mecu.py
+++ mecu.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+
     ### Creating WebDriver ###
 #========================================================================================================================
 
